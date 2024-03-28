0712f655e39076a9f031c0a5f5cb8bba875b32a3
carmona <carmonajca@gmail.com>
2024-03-28 21:15:57 +0000

    Black formatted

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,9 @@
 # -- Project information -----------------------------------------------------
 import sphinx_rtd_theme
 
-project = 'cusim'
-copyright = '2021, Jisang Yoon'
-author = 'Jisang Yoon'
+project = "cusim"
+copyright = "2021, Jisang Yoon"
+author = "Jisang Yoon"
 
 # The full version, including alpha/beta/rc tags
 release = "0.0.1"
@@ -33,14 +33,10 @@ release = "0.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-  "sphinx_rtd_theme",
-  "sphinx.ext.autodoc",
-  "sphinx.ext.napoleon"
-]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -63,7 +59,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_sidebars = {
     "**": [
--- examples/example_w2v.py
+++ examples/example_w2v.py
@@ -43,121 +43,132 @@ EPOCHS = 10
 
 
 def download():
-  if os.path.exists(DATA_PATH):
-    LOGGER.info("%s already exists", DATA_PATH)
-    return
-  if not os.path.exists(DOWNLOAD_PATH):
-    os.makedirs(DOWNLOAD_PATH, exist_ok=True)
-  api.BASE_DIR = DOWNLOAD_PATH
-  filepath = api.load(DATASET, return_path=True)
-  LOGGER.info("filepath: %s", filepath)
-  cmd = ["gunzip", "-c", filepath, ">", DATA_PATH]
-  cmd = " ".join(cmd)
-  LOGGER.info("cmd: %s", cmd)
-  subprocess.call(cmd, shell=True)
-  preprocess_data()
+    if os.path.exists(DATA_PATH):
+        LOGGER.info("%s already exists", DATA_PATH)
+        return
+    if not os.path.exists(DOWNLOAD_PATH):
+        os.makedirs(DOWNLOAD_PATH, exist_ok=True)
+    api.BASE_DIR = DOWNLOAD_PATH
+    filepath = api.load(DATASET, return_path=True)
+    LOGGER.info("filepath: %s", filepath)
+    cmd = ["gunzip", "-c", filepath, ">", DATA_PATH]
+    cmd = " ".join(cmd)
+    LOGGER.info("cmd: %s", cmd)
+    subprocess.call(cmd, shell=True)
+    preprocess_data()
+
 
 def preprocess_data():
-  tokenizer = RegexpTokenizer(r'\w+')
-  nltk.download("wordnet")
-  lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()
-  fout = open(DATA_PATH + ".tmp", "wb")
-  with open(DATA_PATH, "rb") as fin:
-    for line in tqdm.tqdm(fin):
-      line = line.decode("utf8").strip()
-      line = preprocess_line(line, tokenizer, lemmatizer)
-      fout.write((line + "\n").encode("utf8"))
-  fout.close()
-  os.rename(DATA_PATH + ".tmp", DATA_PATH)
+    tokenizer = RegexpTokenizer(r"\w+")
+    nltk.download("wordnet")
+    lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()
+    fout = open(DATA_PATH + ".tmp", "wb")
+    with open(DATA_PATH, "rb") as fin:
+        for line in tqdm.tqdm(fin):
+            line = line.decode("utf8").strip()
+            line = preprocess_line(line, tokenizer, lemmatizer)
+            fout.write((line + "\n").encode("utf8"))
+    fout.close()
+    os.rename(DATA_PATH + ".tmp", DATA_PATH)
+
 
 def preprocess_line(line, tokenizer, lemmatizer):
-  line = line.lower()
-  line = tokenizer.tokenize(line)
-  line = [token for token in line
-          if not token.isnumeric() and len(token) > 1]
-  line = [lemmatizer.lemmatize(token) for token in line]
-  return " ".join(line)
+    line = line.lower()
+    line = tokenizer.tokenize(line)
+    line = [token for token in line if not token.isnumeric() and len(token) > 1]
+    line = [lemmatizer.lemmatize(token) for token in line]
+    return " ".join(line)
+
 
 def run_cusim(skip_gram=False, hierarchical_softmax=False):
-  download()
-  opt = {
-    "data_path": DATA_PATH,
-    "processed_data_dir": PROCESSED_DATA_DIR,
-    # "skip_preprocess": os.path.exists(PROCESSED_DATA_DIR),
-    "num_dims": NUM_DIMS,
-    "epochs": EPOCHS,
-    "word_min_count": MIN_COUNT,
-    "lr": 0.001,
-    "io": {
-      "lower": False
-    },
-    "neg": 0 if hierarchical_softmax else NEG_SIZE,
-    "skip_gram": skip_gram,
-    "cbow_mean": CBOW_MEAN,
-  }
-  start = time.time()
-  w2v = CuW2V(opt)
-  w2v.train_model()
-  elapsed = time.time() - start
-  LOGGER.info("elapsed for cusim w2v training: %.4e sec", elapsed)
-  w2v.save_word2vec_format(CUSIM_MODEL, binary=False)
-  return elapsed, evaluate_w2v_model(CUSIM_MODEL)
+    download()
+    opt = {
+        "data_path": DATA_PATH,
+        "processed_data_dir": PROCESSED_DATA_DIR,
+        # "skip_preprocess": os.path.exists(PROCESSED_DATA_DIR),
+        "num_dims": NUM_DIMS,
+        "epochs": EPOCHS,
+        "word_min_count": MIN_COUNT,
+        "lr": 0.001,
+        "io": {"lower": False},
+        "neg": 0 if hierarchical_softmax else NEG_SIZE,
+        "skip_gram": skip_gram,
+        "cbow_mean": CBOW_MEAN,
+    }
+    start = time.time()
+    w2v = CuW2V(opt)
+    w2v.train_model()
+    elapsed = time.time() - start
+    LOGGER.info("elapsed for cusim w2v training: %.4e sec", elapsed)
+    w2v.save_word2vec_format(CUSIM_MODEL, binary=False)
+    return elapsed, evaluate_w2v_model(CUSIM_MODEL)
+
 
 def run_gensim(skip_gram=False, hierarchical_softmax=False, workers=8):
-  download()
-  start = time.time()
-  model = gensim.models.Word2Vec(corpus_file=DATA_PATH, workers=workers,
-                                 sg=skip_gram, hs=hierarchical_softmax,
-                                 min_alpha=LEARNING_RATE, min_count=MIN_COUNT,
-                                 alpha=LEARNING_RATE, negative=NEG_SIZE,
-                                 iter=EPOCHS, cbow_mean=CBOW_MEAN,
-                                 size=NUM_DIMS)
-  elapsed = time.time() - start
-  LOGGER.info("elapsed for gensim w2v training: %.4e sec", elapsed)
-  model.wv.save_word2vec_format(GENSIM_MODEL, binary=False)
-  LOGGER.info("gensim w2v model is saved to %s", GENSIM_MODEL)
-  return elapsed, evaluate_w2v_model(GENSIM_MODEL)
+    download()
+    start = time.time()
+    model = gensim.models.Word2Vec(
+        corpus_file=DATA_PATH,
+        workers=workers,
+        sg=skip_gram,
+        hs=hierarchical_softmax,
+        min_alpha=LEARNING_RATE,
+        min_count=MIN_COUNT,
+        alpha=LEARNING_RATE,
+        negative=NEG_SIZE,
+        iter=EPOCHS,
+        cbow_mean=CBOW_MEAN,
+        size=NUM_DIMS,
+    )
+    elapsed = time.time() - start
+    LOGGER.info("elapsed for gensim w2v training: %.4e sec", elapsed)
+    model.wv.save_word2vec_format(GENSIM_MODEL, binary=False)
+    LOGGER.info("gensim w2v model is saved to %s", GENSIM_MODEL)
+    return elapsed, evaluate_w2v_model(GENSIM_MODEL)
+
 
 def evaluate_w2v_model(model=GENSIM_MODEL):
-  LOGGER.info("load word2vec format model from %s", model)
-  model = gensim.models.KeyedVectors.load_word2vec_format(model)
-  results = model.wv.evaluate_word_pairs(datapath("wordsim353.tsv"),
-                                         case_insensitive=False)
-  LOGGER.info("evaluation results: %s", results)
-  return results
+    LOGGER.info("load word2vec format model from %s", model)
+    model = gensim.models.KeyedVectors.load_word2vec_format(model)
+    results = model.wv.evaluate_word_pairs(datapath("wordsim353.tsv"), case_insensitive=False)
+    LOGGER.info("evaluation results: %s", results)
+    return results
+
 
 # gpu model variable is for being displayed in markdown
 # please put the real gpu modelname
-def run_experiments(skip_gram=False, hierarchical_softmax=False,
-                    gpu_model="NVIDIA T4"):
-  training_time = {"attr": "training time (sec)"}
-  pearson = {"attr": "pearson"}
-  spearman = {"attr": "spearman"}
-  for i in [1, 2, 4, 8]:
-    elapsed, evals = run_gensim(skip_gram, hierarchical_softmax, i)
-    training_time[f"{i} workers (gensim)"] = elapsed
-    pearson[f"{i} workers (gensim)"] = evals[0][0]
-    spearman[f"{i} workers (gensim)"] = evals[1][0]
-  elapsed, evals = run_cusim(skip_gram, hierarchical_softmax)
-  gpu_title = f"{gpu_model} (cusim)"
-  training_time[gpu_title] = elapsed
-  pearson[gpu_title] = evals[0][0]
-  spearman[gpu_title] = evals[1][0]
-  df0 = pd.DataFrame([training_time, pearson, spearman])
-  df0.set_index("attr", inplace=True)
-  print(df0.to_markdown())
+def run_experiments(skip_gram=False, hierarchical_softmax=False, gpu_model="NVIDIA T4"):
+    training_time = {"attr": "training time (sec)"}
+    pearson = {"attr": "pearson"}
+    spearman = {"attr": "spearman"}
+    for i in [1, 2, 4, 8]:
+        elapsed, evals = run_gensim(skip_gram, hierarchical_softmax, i)
+        training_time[f"{i} workers (gensim)"] = elapsed
+        pearson[f"{i} workers (gensim)"] = evals[0][0]
+        spearman[f"{i} workers (gensim)"] = evals[1][0]
+    elapsed, evals = run_cusim(skip_gram, hierarchical_softmax)
+    gpu_title = f"{gpu_model} (cusim)"
+    training_time[gpu_title] = elapsed
+    pearson[gpu_title] = evals[0][0]
+    spearman[gpu_title] = evals[1][0]
+    df0 = pd.DataFrame([training_time, pearson, spearman])
+    df0.set_index("attr", inplace=True)
+    print(df0.to_markdown())
+
 
 # gpu model variable is for being displayed in markdown
 # please put the real gpu modelname
 def run_various_experiments(gpu_model="NVIDIA T4"):
-  for sg0 in [True, False]:
-    for hs0 in [True, False]:
-      print("=" * 100)
-      LOGGER.info("setting: %s, %s",
-                  "skip gram" if sg0 else "cbow",
-                  "hierarchical softmax" if hs0 else "negative sampling")
-      run_experiments(sg0, hs0, gpu_model)
+    for sg0 in [True, False]:
+        for hs0 in [True, False]:
+            print("=" * 100)
+            LOGGER.info(
+                "setting: %s, %s",
+                "skip gram" if sg0 else "cbow",
+                "hierarchical softmax" if hs0 else "negative sampling",
+            )
+            run_experiments(sg0, hs0, gpu_model)
 
 
 if __name__ == "__main__":
-  fire.Fire()
+    fire.Fire()
